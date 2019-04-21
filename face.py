d008b0e56f8151f689f6550da3a50beafff3e162
Nippun Lakhani <nippun5499@gmail.com>
2019-04-21 17:04:46 +0530

    added

--- face.py
+++ face.py
@@ -4,9 +4,9 @@
 import cv2
 
 # Loading the cascades
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-smile_cascade= cv2.CascadeClassifier('haarcascade_smile.xml')
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_eye.xml')
+smile_cascade= cv2.CascadeClassifier(cv2.data.haarcascades +'haarcascade_smile.xml')
 
 # Defining a function that will do the detections
 
